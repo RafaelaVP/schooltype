562b19ae719d9b0f969b9544b6221a9032674496
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-03-03 17:23:13 -0300

    refact: adjust index

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -18,10 +18,11 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file  => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    console.log(model.name)
     db[model.name] = model;
   });
 
